9b4da5cc2326a4204aa352c8bfa56f53fa97049b
wilaponce@gmail.com <wilaponce@gmail.com>
2025-09-28 00:41:57 -0700

    update

--- backend/Models/Driver.cs
+++ backend/Models/Driver.cs
@@ -1,12 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Cycle2U.Models
 {
     public class Driver
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+
         public bool IsAvailable { get; set; }
+
+        [Range(-90, 90)]
         public double Latitude { get; set; }
+
+        [Range(-180, 180)]
         public double Longitude { get; set; }
     }
 }
- 
--- backend/Models/PickupRequest.cs
+++ backend/Models/PickupRequest.cs
@@ -1,7 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Cycle2U.Models
 {
     public class PickupRequest
     {
-        public string Address { get; set; }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; }
+
+        [Required]
+        public DateTime ScheduledTime { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Location { get; set; }
     }
 }
