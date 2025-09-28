b84859468dc69027367b6c0c0e2085f6fc601f6d
wilaponce@gmail.com <wilaponce@gmail.com>
2025-09-28 02:15:43 -0700

    update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,11 +1,8 @@
-
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Cycle2U.Data;
 using Cycle2U.Models;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 
