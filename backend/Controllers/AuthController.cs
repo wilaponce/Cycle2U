f2e36061dd7c035efab526dbfb58f53e57a3b77e
wilaponce@gmail.com <wilaponce@gmail.com>
2025-09-28 07:20:23 -0700

    update AuthController.cs

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Cycle2U.Models;
+using Cycle2U.ViewModels;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.Text;
