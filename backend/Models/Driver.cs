8ccb0efa5ca61aefb0278609c5ae7ca67250bd1b
wilaponce@gmail.com <wilaponce@gmail.com>
2025-09-27 23:54:43 -0700

    update Driver.cs

---
+++ backend/Models/Driver.cs
@@ -0,0 +1,12 @@
+namespace Cycle2U.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsAvailable { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
+ 
