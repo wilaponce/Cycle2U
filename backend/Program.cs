686b0ebee59c7befde2dcd508934996c39f693b9
wilaponce@gmail.com <wilaponce@gmail.com>
2025-09-27 20:47:27 -0700

    update Program.cs DBcontext

--- backend/Program.cs
+++ backend/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using Cycle2UAuthAPI.Data;
+using Cycle2u.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
     
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<AppDbContext>()
+    .AddEntityFrameworkStores<Application DBcontext>()
     .AddDefaultTokenProviders();
     
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
