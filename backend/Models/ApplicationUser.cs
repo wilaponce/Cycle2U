1b98c3f8089ea8f0d112b524b2761fb979f3a3e0
wilaponce@gmail.com <wilaponce@gmail.com>
2025-09-28 00:12:01 -0700

    update ApplicationUser.cs

--- backend/Models/ApplicationUser.cs
+++ backend/Models/ApplicationUser.cs
@@ -5,9 +5,7 @@ namespace Cycle2U.Models
 {
     public class ApplicationUser: IdentityUser
     {
-        public int Id { get; set; }
-        public string Email { get; set; }
-        public string PasswordHash { get; set; }
+        
         public string Name { get; set; }
         public List<RefreshToken> RefreshTokens { get; set; } = new();
     }
