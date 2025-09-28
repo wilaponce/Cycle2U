d90351582a4724ad8506105a5c1e6699ba5f4866
wilaponce@gmail.com <wilaponce@gmail.com>
2025-09-27 20:51:56 -0700

    update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -12,8 +12,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
     
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<Application DBcontext>()
-    .AddDefaultTokenProviders();
+    .AddEntityFrameworkStores<Application DBcontext>().AddDefaultTokenProviders();
     
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
