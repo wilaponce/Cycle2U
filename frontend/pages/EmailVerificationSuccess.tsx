917b5f78ebc6b5e01caea816af9443364d897fa9
wilton ponce <wilaponce@gmail.com>
2025-09-28 14:16:54 -0700

    Update EmailVerificationSuccess.tsx

--- frontend/pages/EmailVerificationSuccess.tsx
+++ frontend/pages/EmailVerificationSuccess.tsx
@@ -15,8 +15,7 @@ export default function EmailVerificationSuccess() {
           <p className="text-gray-700 mb-6">
             Your email address has been successfully verified. You can now access your account.
           </p>
-          /login
-            <Link className="inline-block text-white bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
+            <Link href="/login" className="inline-block text-white bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
               Go to Login
             </Link>
         </div>
