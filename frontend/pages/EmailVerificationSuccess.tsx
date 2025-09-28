161fc1f8986da693338c8129ea8f2ab5970f7a9d
wilton ponce <wilaponce@gmail.com>
2025-09-28 14:00:29 -0700

    Update EmailVerificationSuccess.tsx

--- frontend/pages/EmailVerificationSuccess.tsx
+++ frontend/pages/EmailVerificationSuccess.tsx
@@ -18,8 +18,7 @@ export default function EmailVerificationSuccess() {
           /login
             <a className="inline-block text-white bg-blue-600 hover:bg-blue-700 px-4 py-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
               Go to Login
-            </a>
-          </Link>
+            </a></Link>
         </div>
       </main>
     </>
