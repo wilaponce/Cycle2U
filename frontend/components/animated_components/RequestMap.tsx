f4010a67c5e719500b5b9b93813fc1f61803814d
wilaponce <wilaponce@gmail.com>
2025-10-04 05:16:11 +0000

    streetview Maptiles

--- frontend/components/animated_components/RequestMap.tsx
+++ frontend/components/animated_components/RequestMap.tsx
@@ -25,7 +25,7 @@ export default function RequestMap({ requests }: Props) {
 
         const map = new maplibregl.Map({
             container: mapRef.current,
-            style: 'https://demotiles.maplibre.org/style.json',
+            style: 'https://api.maptiler.com/maps/streets/style.json?key=${process.env.NEXT_PUBLIC_MAPTILER_API_KEY}',
             center: [0, 22.54992],
             zoom: 3,
         });
