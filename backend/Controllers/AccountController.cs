bbae688ad8451bfeedfb5edbe9ed4600cb9d2d06
wilaponce <wilaponce@gmail.com>
2025-10-05 13:03:58 -0700

    update AccountController.cs

--- backend/Controllers/AccountController.cs
+++ backend/Controllers/AccountController.cs
@@ -7,7 +7,7 @@ using Cycle2U.Data;
 using Cycle2U.ViewModels;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
-
+using Cycle2U.Services;
 namespace Cycle2U.Controllers
 {
     [Route("api/[controller]")]
