5ef01fa7ce33bd8e4cf806ab3cd340bb451bcba8
wilaponce <wilaponce@gmail.com>
2025-10-09 20:18:26 +0000

    docker fil ef database

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,57 +1,38 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 using Cycle2U.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Add DbContext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-    
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.Authority = "https://securetoken.google.com/cycle2u-70881949-3273a";
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidIssuer = "https://securetoken.google.com/cycle2u-70881949-3273a",
-            ValidateAudience = true,
-            ValidAudience = "cycle2u-70881949-3273a",
-            ValidateLifetime = true
-        };
-    });
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("WebAppPolicy", policy =>
-    {
-        policy.WithOrigins("http://localhost:3000", "https://*.cycle2u.com")
-              .AllowAnyHeader()
-              .AllowAnyMethod();
-    });
-});
-
-builder.Services.AddControllers();
-builder.Services.AddAuthorization();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    options.UseSqlite(connectionString));
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cycle2U API V1");
-    });
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-app.UseAuthentication();
+app.UseStaticFiles();
+
+app.UseRouting();
+
 app.UseAuthorization();
-app.UseCors("WebAppPolicy");
-app.MapControllers();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
